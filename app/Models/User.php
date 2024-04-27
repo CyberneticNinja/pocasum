93feb36a65c8fa0e57dc39c592151cec867f1f1d
Stephen Abrokwah <AbrokwahS@gmail.com>
2024-04-27 13:17:56 -0400

    migrations, ands models added

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
