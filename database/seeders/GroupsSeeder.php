24759e2b4bb4fce7c5077b7f22c5b3e8f3c98d53
Stephen Abrokwah <AbrokwahS@gmail.com>
2024-04-28 00:52:32 -0400

    -Modified GroupsSeeder.php

--- database/seeders/GroupsSeeder.php
+++ database/seeders/GroupsSeeder.php
@@ -16,7 +16,7 @@ class GroupsSeeder extends Seeder
         DB::table('groups')->insert([
             ['name' => 'Youth Fellowship', 'church_id' => 1],
             ['name' => 'Young couples Ministry', 'church_id' => 1],
-            ['name' => 'Men’s Fellowship', 'church_id' => 2],
+            ['name' => 'Older parents group', 'church_id' => 2],
             ['name' => 'Sunday School', 'church_id' => 2],
             ['name' => 'Choir', 'church_id' => 3],
             ['name' => 'Bible Study Group', 'church_id' => 3],
